20873a67369877b317af23ca1c1d494e493bec91
liliana Quinley <lilicuellar99@gmail.com>
2018-12-04 00:35:15 -0700

    post updated and create functions

--- config/orm.js
+++ config/orm.js
@@ -9,9 +9,28 @@ var orm = {
       result) { 
         if (err) throw err;
         cb(result)
+        console.log("this is the orm all ")
      })
-    }
+    },
+
+  update: function (tableInput, condition, cb) {
+    console.log("UPDATE condigion is " + condition + " taleInput is " + tableInput)
+    connection.query("UPDATE " + tableInput + ' SET devoured=true WHERE id='+condition+';',
+     function (err, result) {
+      if (err) throw err;
+      cb(result);
+      console.log("this is the UPDATED Func");
+      })
+    },
+
+    create: function (tableInput, val, cb) { 
+      connection.query('INSERT INTO '+ tableInput + "(burger_name) VALUES ('"+val+"') ; ", function (err, result) {
+          if (err) throw err;
+          cb(result);
+      })
+     }
 }
+
 module.exports = orm;
 // Routes
 // =============================================================
--- models/burger.js
+++ models/burger.js
@@ -3,11 +3,17 @@
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 var orm = require("../config/orm.js");
 var burger = {
- all: function (cb) {
-    orm.all("burgers", function (res) { 
-        cb(res)
-     }) 
-    }
+    all: function (cb) {
+        orm.all("burgers", function (res) { 
+            cb(res)
+        }) 
+    },
+
+    update: function (id,cb) {
+        orm.update('burgers', id,cb);
+      }
+
+
 }
 
 // Export at the end of the burger.js file.
