794c64f01815a6abd1e99240a674798e3de63f4c
liliana Quinley <lilicuellar99@gmail.com>
2018-12-08 11:24:15 -0700

    heroku changes

--- server.js
+++ server.js
@@ -5,31 +5,33 @@ var exphbs = require("express-handlebars");
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var routes = require('./controllers/burgers_controller.js');
 
-app.use(bodyParser.urlencoded ({
-  extended: false
-
-}))
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+
+}))
+
+
 // 
 app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({
-   defaultLayout: "main" 
-  }));
+  defaultLayout: "main"
+}));
 
 app.set("view engine", "handlebars");
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+app.use(routes);
 
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
